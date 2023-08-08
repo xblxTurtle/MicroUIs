82c7f5c59ba80e5fe1cf8f52b737fe612ae61095
Pavel Sergeyev <xblxTurtle@gmail.com>
2023-08-08 22:43:41 +0100

    publicPath

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -7,7 +7,8 @@ const packageJson = require('../package.json');
 const prodConfig = {
     mode:'production',
     output: {
-        filename: "[name].[contenthash].js"
+        filename: "[name].[contenthash].js",
+        publicPath: "container/latest/"
     },
     plugins: [
         new ModuleFederationPlugin({
