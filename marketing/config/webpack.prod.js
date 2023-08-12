a015f8637bd91f8ef98ede159998a4a0c87a9db6
Pavel Sergeyev <xblxTurtle@gmail.com>
2023-08-12 21:42:43 +0100

    add marketing deployment

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -6,7 +6,8 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const prodConfig = {
     mode:'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: "marketing/latest/"
     },
     plugins: [
         new ModuleFederationPlugin({
